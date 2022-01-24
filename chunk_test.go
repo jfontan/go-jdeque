4d5a45cbb2fabc04d240eced52ca0c0127dc7284
Javi Fontan <jfontan@gmail.com>
2022-01-24 23:02:15 +0100

    Test mixed front and back chunk

--- chunk_test.go
+++ chunk_test.go
@@ -49,3 +49,45 @@ func TestChunkBack(t *testing.T) {
 	require.False(t, ok)
 	require.Equal(t, 0, v)
 }
+
+func TestChunkMixed(t *testing.T) {
+	c := NewChunk[int](10)
+
+	for i := 0; i < 5; i++ {
+		ok := c.PushFront(i)
+		require.True(t, ok)
+	}
+
+	for i := 0; i < 4; i++ {
+		v, ok := c.PopBack()
+		require.True(t, ok)
+		require.Equal(t, i, v)
+	}
+
+	for i := 3; i >= 0; i-- {
+		ok := c.PushBack(i)
+		require.True(t, ok)
+	}
+
+	ok := c.PushBack(-1)
+	require.False(t, ok)
+
+	for i := 5; i < 10; i++ {
+		ok := c.PushFront(i)
+		require.True(t, ok)
+	}
+
+	ok = c.PushFront(10)
+	require.False(t, ok)
+
+	for i := 0; i < 10; i++ {
+		v, ok := c.PopBack()
+		require.True(t, ok)
+		require.Equal(t, i, v)
+	}
+
+	_, ok = c.PopFront()
+	require.False(t, ok)
+	_, ok = c.PopBack()
+	require.False(t, ok)
+}
