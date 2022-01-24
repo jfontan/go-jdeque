55addc4b2ce027a0b0974e9eff197748e4df1fa5
Javi Fontan <jfontan@gmail.com>
2022-01-24 22:50:00 +0100

    Chunk implementation for back

--- chunk.go
+++ chunk.go
@@ -26,9 +26,13 @@ func (c *Chunk[T]) pos() int {
 }
 
 func (c *Chunk[T]) PushFront(value T) bool {
+	if c.empty() && c.start > 0 {
+		c.start = 0
+	}
 	if !c.canPushFront() {
 		return false
 	}
+
 	c.values[c.pos()] = value
 	c.size++
 	return true
@@ -42,5 +46,35 @@ func (c *Chunk[T]) PopFront() (T, bool) {
 
 	v = c.values[c.pos()-1]
 	c.size--
+	return v, true
+}
+
+func (c *Chunk[T]) canPushBack() bool {
+	return c.start > 0
+}
+
+func (c *Chunk[T]) PushBack(value T) bool {
+	if c.empty() && c.start < (len(c.values)-1) {
+		c.start = len(c.values)
+	}
+	if !c.canPushBack() {
+		return false
+	}
+
+	c.start--
+	c.size++
+	c.values[c.start] = value
+	return true
+}
+
+func (c *Chunk[T]) PopBack() (T, bool) {
+	var v T
+	if c.empty() {
+		return v, false
+	}
+
+	v = c.values[c.start]
+	c.size--
+	c.start++
 	return v, true
 }
--- chunk_test.go
+++ chunk_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestChunk(t *testing.T) {
+func TestChunkFront(t *testing.T) {
 	c := NewChunk[int](10)
 
 	for i := 0; i < 10; i++ {
@@ -24,6 +24,28 @@ func TestChunk(t *testing.T) {
 	}
 
 	v, ok := c.PopFront()
+	require.False(t, ok)
+	require.Equal(t, 0, v)
+}
+
+func TestChunkBack(t *testing.T) {
+	c := NewChunk[int](10)
+
+	for i := 0; i < 10; i++ {
+		ok := c.PushBack(i)
+		require.True(t, ok)
+	}
+
+	ok := c.PushBack(10)
+	require.False(t, ok)
+
+	for i := 9; i >= 0; i-- {
+		v, ok := c.PopBack()
+		require.True(t, ok)
+		require.Equal(t, i, v)
+	}
+
+	v, ok := c.PopBack()
 	require.False(t, ok)
 	require.Equal(t, 0, v)
 }
